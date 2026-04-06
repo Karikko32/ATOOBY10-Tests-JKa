d620a5e08c5c4980147d262dc5ab54052b4c6c2d
Juuso <juuso.j.karikorpi@student.lab.fi>
2026-04-06 22:05:33 +0300

    Fix nyc-entry: remove test requires

--- nyc-entry.cjs
+++ nyc-entry.cjs
@@ -2,12 +2,3 @@ require("@babel/register")({
   extensions: [".js"],
   ignore: [/node_modules/]
 });
-
-// Lataa testit Mocha-tyyliin
-require("./test/add.test.js");
-require("./test/camelCase.test.js");
-require("./test/divide.test.js");
-require("./test/endsWith.test.js");
-require("./test/eq.test.js");
-require("./test/isArrayLike.test.js");
-// lisää loput testit tähän
